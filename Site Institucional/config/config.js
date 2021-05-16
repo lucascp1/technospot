c5d52fba5254f9ac1b37961bf1a6ea9b9a99639f
Lucas Castrillo <lucas.castrilo33@gmail.com>
2021-05-16 19:42:25 -0300

    Configuração API

---
+++ Site Institucional/config/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  // Insira aqui seus dados do banco NA NUVEM AZURE
+  production: {
+    // altere APENAS username, password, database e host.
+    username: 'freshdragon',
+    password: 'Pitaia@123',
+    database: 'dbTemperatura',
+    host: 'LINK PARA SEU BD, ALGO COMO minhadatabase.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  },
+
+  // Insira aqui seus dados do banco LOCAL - MySQL Workbench
+  dev: {
+    // altere APENAS username, password e database.
+    username: 'technospot',
+    password: 'techno123',
+    database: 'TechnoSpot',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  },
+};
